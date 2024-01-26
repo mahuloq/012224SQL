de2562796ee4edb9a16e04e6bb3b4c677161ac50
mahuloq <95161346+mahuloq@users.noreply.github.com>
2024-01-26 15:14:51 -0600

    Create lesson_three.sql

---
+++ SQLbolt/lesson_three.sql
@@ -0,0 +1,11 @@
+SELECT title, director FROM movies 
+WHERE title LIKE "Toy Story%";
+
+SELECT title, director FROM movies 
+WHERE director LIKE "John Lasseter";
+
+SELECT title, director FROM movies 
+WHERE director NOT LIKE "John Lasseter";
+
+SELECT title, director FROM movies 
+WHERE title LIKE "Wall-E%";
