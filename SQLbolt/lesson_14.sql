858361f494f05c250bfa4e68796d12581d3a020d
mahuloq <95161346+mahuloq@users.noreply.github.com>
2024-01-27 02:37:58 -0600

    Create lesson_14.sql

---
+++ SQLbolt/lesson_14.sql
@@ -0,0 +1,12 @@
+UPDATE Movies
+SET Director = 'John Lasseter'
+WHERE title = "A Bug's Life";
+
+Update Movies
+set year = 1999
+WHERE title = "Toy Story 2";
+
+Update Movies
+set director = "Lee Unkrich", 
+title = 'Toy Story 3'
+WHERE title = "Toy Story 8";
