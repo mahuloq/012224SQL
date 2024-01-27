730892e3d1b32ccb3e06be05365415a968de8d3a
mahuloq <95161346+mahuloq@users.noreply.github.com>
2024-01-27 02:14:31 -0600

    Create lesson_13.sql

---
+++ SQLbolt/lesson_13.sql
@@ -0,0 +1,4 @@
+insert into Movies (title,director,year,length_minutes) values ('Toy Story 4','Bob Mcfarlane',2020,61);
+SELECT * FROM movies;
+
+Insert Into Boxoffice (movie_id,rating, domestic_sales, International_sales)values (15,8.7,340000000,270000000);
