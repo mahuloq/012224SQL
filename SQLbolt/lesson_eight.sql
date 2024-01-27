57deb780002d26949f9b14c3dd00c152cbcc8953
mahuloq <95161346+mahuloq@users.noreply.github.com>
2024-01-27 00:30:20 -0600

    Create lesson_eight.sql

---
+++ SQLbolt/lesson_eight.sql
@@ -0,0 +1,8 @@
+SELECT name, role FROM employees
+WHERE building IS NULL;
+
+SELECT DISTINCT building_name
+FROM buildings 
+LEFT JOIN employees
+    ON building_name = building
+WHERE building IS NULL;
